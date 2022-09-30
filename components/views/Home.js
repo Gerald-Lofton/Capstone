a69068bd744fde110f33187a2118a9029a2001ee
Gerald-Lofton <geraldlofton0702@gmail.com>
2022-09-29 22:15:02 -0500

    Implemented OpenWeatherMap API and added redriects, .env, Added text on Homescreen with grabbed API data

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,5 +1,12 @@
 import html from "html-literal";
 
 export default (state) => html`
-  <section></section>
+  <section>
+    <h1>Homework OpenWeatherMap API output:</h1>
+    <h3>
+      The weather in ${state.weather.city} is ${state.weather.description}.
+      Temperature is ${state.weather.temp}F, and it feels like
+      ${state.weather.feelsLike}F.
+    </h3>
+  </section>
 `;
--- store/Home.js
+++ store/Home.js
@@ -1,4 +1,5 @@
 export default {
   header: "FloodWeather",
   view: "Home",
+  weather: {},
 };
