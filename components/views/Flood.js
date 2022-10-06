a145c771c4bb908a15e7e82bd407349f87394fe0
Gerald-Lofton <geraldlofton0702@gmail.com>
2022-10-06 18:45:51 -0500

    Added NOAA Flood Map iFrame to Flood page

--- components/views/Flood.js
+++ components/views/Flood.js
@@ -1,5 +1,15 @@
 import html from "html-literal";
 
 export default (state) => html`
-  <section></section>
+  <section>
+    <div class="floodIframe">
+      <iframe
+        src="https://water.weather.gov/ahps/region_iframe.php?state=MOsscale=true"
+        height="945"
+        width="1125"
+        frameborder="0"
+        scrolling="no"
+      ></iframe>
+    </div>
+  </section>
 `;
