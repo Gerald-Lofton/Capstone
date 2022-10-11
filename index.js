8062743087725bed8dabbc0ba67c9cc8e9ab81b7
Gerald-Lofton <geraldlofton0702@gmail.com>
2022-10-11 16:31:30 -0500

    no changes to api data from last commit

--- index.js
+++ index.js
@@ -65,10 +65,11 @@ router.hooks({
       case "Flood":
         axios
           .get(
-            `https://history.openweathermap.org/data/2.5/history/city?lat=41.85&lon=-87&type=hour&start=1643720400&end=1643806800&units=imperial&appid=${process.env.OPEN_WEATHER_MAP_API_KEY}`
+            `https://history.openweathermap.org/data/2.5/history/city?lat=41.85&lon=-87&type=hour&start=1643720400&end=1643806800&units=imperial&appid=${process.env.OPEN_WEATHER_HISTORY_API_KEY}`
           )
           .then((response) => {
-            console.log([response.data.list]);
+            store.Flood.weatherHistory = response.data;
+            console.log(store.Flood.weatherHistory);
             done();
           })
           .catch((error) => {
