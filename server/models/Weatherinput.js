8442b3f3e14162c28b3cce464c9fff5175d01bbb
Gerald-Lofton <geraldlofton0702@gmail.com>
2022-10-12 20:11:51 -0500

    fixed Weatherinput schema

--- server/models/Weatherinput.js
+++ server/models/Weatherinput.js
@@ -4,16 +4,12 @@ const WeatherinputSchema = new mongoose.Schema({
   customer: {
     type: String,
     required: true,
-    validate: /^[A-Za-z0-9 ]*$/,
+    // validate: /^[A-Za-z0-9 ]*$/,
   },
-  rain: {
+  input: {
     type: String,
     required: true,
-    enum: ["Yes", "No"],
-  },
-  current: {
-    type: String,
-    validate: /^[A-Za-z0-9 ]*$/,
+    // validate: /^[A-Za-z0-9 ]*$/,
   },
 });
 const Weatherinput = mongoose.model("Weatherinput", WeatherinputSchema);
